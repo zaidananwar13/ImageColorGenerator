2f54fb07bedea8ea37de46f2036d98818a1dd9cb
Zaidan Anwar <zaidan20ti@mahasiswa.pcr.ac.id>
2023-01-01 12:15:42 +0700

    Initial Commit

--- app.py
+++ app.py
@@ -4,9 +4,18 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+app = Flask(__name__)
 
 app = Flask(__name__)
 
